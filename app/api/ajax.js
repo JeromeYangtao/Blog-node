8a77a2a992c2ce38ff92aba41083af41b56ad60c
Thomson <779387850@qq.com>
2017-08-18 16:11:45 +0800

    抽离ajax请求

---
+++ app/api/ajax.js
@@ -0,0 +1,10 @@
+//处理ajax请求
+let Router = require('./router')
+//获取分类列表
+Router.get('/categoryList.action', ctx => {})
+// 增加分类
+Router.get('/category.action', ctx => {return {a: 2}})
+// 添加博客
+Router.post('/blog.action', ctx => {})
+
+module.exports = Router
--- app/api/index.js
+++ app/api/index.js
@@ -1,10 +1,5 @@
 let Router = require('./router')
-//获取分类列表
-Router.get('/categoryList.action', ctx => {})
-// 增加分类
-Router.get('/category.action', ctx => {return {a: 2}})
-// 添加博客
-Router.post('/blog.action', ctx => {})
+require('./ajax')
 module.exports = (ctx) => {
   let {method} = ctx.req
   let {reqCtx, res, resCtx} = ctx
