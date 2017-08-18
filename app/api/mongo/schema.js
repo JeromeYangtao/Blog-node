8aadb746ef08536d1baf6fef44f98e524e3367bd
Thomson <779387850@qq.com>
2017-08-18 16:52:58 +0800

    建立Schema

---
+++ app/api/mongo/schema.js
@@ -0,0 +1,14 @@
+// 创建schema
+const {Schema} = require('mongoose')
+// 创建博客的数据存储 schema
+exports.blogSchema = new Schema({
+  title: String,
+  content: String,//html
+  rawContent: String,//markdown
+  category: String,//分类
+  date: {
+    type: String, default: () => {
+      return new Date().toLocaleString()
+    }
+  }
+})
