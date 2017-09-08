f73f2fe55b98f4f03c30c983c184f34ddfe15194
Thomson <779387850@qq.com>
2017-09-08 17:06:53 +0800

    网站的尾部

--- public/src/App.vue
+++ public/src/App.vue
@@ -1,32 +1,44 @@
 <template>
   <div id="app">
     <nav>
+      <div class="logo"><img src="./assets/logo.png" alt="这里以后会放个logo"></div>
       <ul>
         <li>
-          <router-link to="/">首页</router-link>
+          <router-link to="/">
+            <el-button type="text">首页</el-button>
+          </router-link>
         </li>
         <li>
-          <router-link to="/list">目录</router-link>
+          <router-link to="/list">
+            <el-button type="text">目录</el-button>
+
+          </router-link>
         </li>
         <li>
-          <router-link to="/about">关于作者</router-link>
+          <router-link to="/about">
+            <el-button type="text"> 关于作者</el-button>
+          </router-link>
         </li>
         <li>
-          <router-link to="/write">写博客</router-link>
+          <router-link to="/write">
+            <el-button type="text">写博客</el-button>
+          </router-link>
         </li>
       </ul>
     </nav>
 
     <!--路由部分-->
-    <router-view></router-view>
+    <div class="main">
+      <router-view></router-view>
+    </div>
 
 
-    登录页面
-    <el-button type="primary">主要按钮</el-button>
+    <footer>
+
+      <p class="tm-copyright-text">Copyright &copy; 2017 Thomson | 杨涛</p>
+    </footer>
 
 
-    管理页面
-    <el-button type="primary">上传<i class="el-icon-upload el-icon--right"></i></el-button>
   </div>
 </template>
 
@@ -41,15 +53,40 @@
 
 <style scoped lang="scss">
   #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-
-    nav > ul {
-
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    .logo {
+      text-align: center;
+    }
+    nav {
+      > ul {
+        display: flex;
+        > li {
+          transition: all 0.6s;
+          &:hover {
+            background: #ddd;
+          }
+          > a {
+            display: inline-block;
+            padding: 16px 24px;
+          }
+        }
+      }
+    }
+    .main{
+      width: 80vw;
+      text-align: center;
+      border: 1px solid black;
+      border-radius: 5px;
+    }
+    footer {
+      background: #ddd;
+      width: 100vw;
+      height: 8vh;
+      display: flex;
+      justify-content: center;
+      align-items: center;
     }
 
   }
