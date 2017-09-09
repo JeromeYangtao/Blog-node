de899b67c28aed6daed0268705425396e5a1edce
Thomson <779387850@qq.com>
2017-09-09 14:40:05 +0800

    接口往REST方向靠拢

--- app/api/ajax.js
+++ app/api/ajax.js
@@ -2,34 +2,30 @@
 let Router = require('./router')
 let {$_saveCategory, $_getCategoryList, $_removeCategory} = require('./mongo/category')
 let {$_saveBlog, $_getBlogList, $_removeBlog} = require('./mongo/blog')
-//获取分类列表
-Router.get('/categoryList.action', ctx => {
+//博客分类
+Router.get('/category.action', ctx => {
   return $_getCategoryList(ctx.reqCtx.query)
 })
-// 增加分类
-Router.get('/createCategory.action', ctx => {
-  let category = ctx.reqCtx.query
+Router.post('/category.action', ctx => {
+  let category = ctx.reqCtx.body
   return $_saveCategory(category)
 })
-// 删除分类
-Router.delete('/removeCategory.action', ctx => {
+Router.delete('/category.action', ctx => {
   let condition = ctx.reqCtx.query
   return $_removeCategory(condition)
 })
 
-// 添加博客
+//博客
+Router.get('/blog.action', ctx => {
+  return $_getBlogList(ctx.reqCtx.query)
+})
 Router.post('/blog.action', ctx => {
   let blog = ctx.reqCtx.body
   return $_saveBlog(blog)
 })
-// 删除博客
-Router.delete('/removeBlog.action', ctx => {
+Router.delete('/blog.action', ctx => {
   let condition = ctx.reqCtx.query
   return $_removeBlog(condition)
-})
-//获取博客列表
-Router.get('/blogList.action', ctx => {
-  return $_getBlogList(ctx.reqCtx.query)
 })
 
 module.exports = Router
